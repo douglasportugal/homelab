6c7bafeb5df286adff7f6cec3b7be13b04d126d9
Douglas Portugal <douglfabian@gmail.com>
2025-11-21 14:27:39 -0300

    k8s-proxmox: através de um makefile

---
+++ k8s-proxmox/terraform/k8s/locals.tf
@@ -0,0 +1,63 @@
+locals {
+    scsihw      = "virtio-scsi-pci"
+    cpu_type    = "host"
+    clone       = "ubuntu-2204-cloud-init"
+    agent       = 1
+    nameserver  = "192.168.0.235"
+    sshkeys     = file("~/.ssh/id_rsa.pub")
+    private_key = file("~/.ssh/id_rsa")
+    target_node = "pve"
+    cidr        = "192.168.0.0/24"
+    onboot      = true
+    
+    network = {
+        bridge  = "vmbr0"
+        model   = "virtio"
+    }
+    
+    serial = {
+        type    = "socket"
+    }
+
+    disks = {
+        main = {
+            backup  = true
+            format  = "raw"
+            type    = "disk"
+            storage = "local-lvm"
+            slot    = "scsi0"
+            discard = true
+        }
+        cloudinit = {
+            backup  = true
+            format  = "raw"
+            type    = "cloudinit"
+            storage = "local-lvm"
+            slot    = "ide2"
+        }
+    }
+
+    masters = {
+        memory  = 3192
+        cores   = 2
+        sockets = 1
+        ciuser  = "ubuntu"
+        vmid    = 800
+        size    = "30G"
+        count   = 1
+        name    = "master"
+        last_octect = 80
+    }
+
+    workers = {
+        memory  = 3192
+        cores   = 2
+        sockets = 1
+        ciuser  = "ubuntu"
+        vmid    = 900
+        size    = "30G"
+        count   = 2
+        name    = "worker"
+        last_octect = 90
+    }
+}
---
+++ k8s-proxmox/terraform/k8s/workers.tf
@@ -0,0 +1,76 @@
+resource "proxmox_vm_qemu" "workers" {
+    count = local.workers.count
+    nameserver =  local.nameserver
+    vmid = local.workers.vmid + count.index
+    ciuser = local.workers.ciuser
+    sshkeys = local.sshkeys
+    agent = local.agent
+    clone = local.clone
+    cores = local.workers.cores
+    sockets = local.workers.sockets
+    cpu_type = local.cpu_type
+    memory = local.workers.memory
+    scsihw = local.scsihw
+    target_node = local.target_node
+    onboot = local.onboot
+
+    name = format(
+        "%s-%s",
+        local.workers.name,
+        count.index
+    )
+
+    network {
+        id        = 0
+        bridge    = local.network.bridge
+        model     = local.network.model
+    }
+
+    ipconfig0 = format(
+        "ip=%s/24,gw=%s",
+        cidrhost(
+        local.cidr,
+        local.workers.last_octect + count.index
+        ),
+        cidrhost(local.cidr, 1)
+    )
+
+    serial {
+        id   = 0
+        type = local.serial.type
+    }
+
+    disk {
+        backup  = local.disks.cloudinit.backup
+        format  = local.disks.cloudinit.format
+        type    = local.disks.cloudinit.type
+        storage = local.disks.cloudinit.storage
+        slot    = local.disks.cloudinit.slot
+    }
+
+    disk {
+        backup  = local.disks.main.backup
+        format  = local.disks.main.format
+        type    = local.disks.main.type
+        storage = local.disks.main.storage
+        slot    = local.disks.main.slot
+        discard = local.disks.main.discard
+        size    = local.workers.size
+    }
+    
+    connection {
+        type        = "ssh"
+        user        = local.workers.ciuser
+        private_key = local.private_key
+        host = cidrhost(
+        local.cidr,
+        local.workers.last_octect + count.index
+        )
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+        "cloud-init status --wait"
+        ]
+    }
+}
